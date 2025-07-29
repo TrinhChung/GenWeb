d42497528d8f4c8cbe1616124c3e984f0bc5fd50
Trinh Văn Chung <75870812+TrinhChung@users.noreply.github.com>
2025-07-29 14:58:03 +0900

    Fix responsive width for category cards

--- template_fe/template_default/src/components/CategoryItem.tsx
+++ template_fe/template_default/src/components/CategoryItem.tsx
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 const CategoryItem = ({ categoryTitle, image, link } : { categoryTitle: string; image: string; link: string; }) => {
   return (
-    <div className="w-[600px] relative max-[1250px]:w-[400px] max-[1250px]:h-[400px] max-sm:w-[300px] max-sm:h-[300px]">
+    <div
+      className="relative w-full sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] xl:w-[600px]"
+    >
       <Link to={`/shop/${link}`}>
-      <img src={`/src/assets/${image}`} className="h-full w-full" />
+      <img
+        src={`/src/assets/${image}`}
+        className="h-full w-full object-cover"
+      />
       <div className="bg-secondaryBrown text-white absolute bottom-0 w-full h-16 flex justify-center items-center max-sm:h-12">
         <h3 className="text-2xl max-sm:text-xl">{ categoryTitle }</h3>
       </div>
